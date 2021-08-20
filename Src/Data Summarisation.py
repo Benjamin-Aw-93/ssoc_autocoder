9e69306f5798e65e4bb4f9aefe0a11f99a0fe500
benjamin-aw-93 <benjaminaw93@hotmail.com>
2021-08-20 16:32:09 +0800

    Removed unused packages (torch) first

--- Src/Data Summarisation.py
+++ Src/Data Summarisation.py
@@ -1,20 +1,36 @@
 # Importing required libraries
 import numpy as np
 import pandas as pd
-import torch
-import torch.nn.functional as F
-from torch.utils.data import Dataset, DataLoader, RandomSampler, SequentialSampler
-
-# Importing the T5 modules from huggingface/transformers
-from transformers import T5Tokenizer, T5ForConditionalGeneration
-
-# Setting up the device if GPU is available
-from torch import cuda
-device = 'cuda' if cuda.is_available() else 'cpu'
-
+import re
 
 # Try to get summarised data out using HTML tags
 
 mcf_df = pd.read_csv("..\Data\Processed\WGS_Dataset_JobInfo_precleaned.csv")
 
-mcf_df
+# Temp: Make dataset smaller so that it is easier to work with
+mcf_df.head()
+
+mcf_df = mcf_df[["Title", "Description", "SSOC"]].sample(frac=0.1)
+
+
+# Naive way of extracting
+def extracting_job_desc_naive(text):
+    '''
+
+    Extract job description using <li> HTML tags
+
+    Parameters:
+        text (str): Selected text
+
+    Returns:
+        list_extracted_text(text) : Text with html tags <li>
+
+    '''
+
+    pattern = re.compile('(?<=<li>).*?(?=</li)')
+    return pattern.findall(text)
+
+
+mcf_df['Description naive'] = mcf_df['Description'].apply(extracting_job_desc_naive)
+
+mcf_df['Count'] =
