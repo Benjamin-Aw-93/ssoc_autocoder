87f4ec305f145c91f9b09da29566d27ad88405b7
benjamin-aw-93 <benjaminaw93@hotmail.com>
2021-08-22 21:31:56 +0800

    Adding in second data cleaning method

--- Src/Data Summarisation.py
+++ Src/Data Summarisation.py
@@ -27,6 +27,25 @@ class extraction_text:
         self.subsample = df_text.head(100)
 
 
+# Obtained from Data Cleaning.py, write main function for that, import and remove
+def remove_html_tags_newline(text):
+    '''
+
+    Remove html tags from a string with generic regex
+
+    Parameters:
+        text (str): Selected text
+
+    Returns:
+        cleaned_text(text) : Text with html tags and new line  removed
+
+    '''
+
+    clean = re.compile('<.*?>')
+    newline_clean = re.compile('\n')
+    return re.sub(newline_clean, ' ', re.sub(clean, '', text)).lower()
+
+
 # Naive way of extracting
 def extracting_job_desc_naive(text):
     '''
@@ -41,12 +60,45 @@ def extracting_job_desc_naive(text):
 
     '''
 
-    pattern = re.compile('(?<=<li>).*?(?=</li)')
+    pattern = re.compile('(?<=<li>).*?(?=</li>)')
     return pattern.findall(text)
+
+
+# Extracting based on finding the word description
+def extracting_job_desc_named(text, lst_words):
+    '''
+
+    Extract job description using specific header text
+
+    Parameters:
+        text (str): Selected text
+        lst_words (list): List of header words to look out for
+
+    Returns:
+        list_extracted_text(text) : Extracted text
+
+    '''
+    output = []
+
+    for word in lst_words:
+        pattern = re.compile('(?i)(?<=' + word + ').*?(?=<strong>)')
+        search = pattern.findall(text)
+        output.append(search)
+
+    flat_list = [remove_html_tags_newline(item) for sublist in output for item in sublist]
+
+    return flat_list
+
+
+txt = '<p><strong>Role Descriptions:</strong></p> <p>This position reports to the Commercial Planning &amp; Operations (CPO) manager, and interfaces with OBFS (Own Brands &amp; Food Solutions) staff and product managers.</p> <p><br></p> <p><strong>Specific Responsibilities:</strong></p> <p>Individual will be involved in:</p> <ol> <li>Sourcing process - building long list of suppliers, preparing RFI, RFP, contacting suppliers for the roll out of existing and new products</li> <li>Conducting market-price research, generating price-match proposals for Own Brand portfolio</li> </ol> <p><strong>Technical Skills and Competencies:</strong></p> <ul> <li>Project management, including how to engage stakeholders professionally to deliver project targets</li> <li>Active participation in the sourcing process</li> <li>Participation in price planning process, including contributing to and pitching price-match proposals to stakeholders</li> </ul> <p><strong>Duration of Traineeships:</strong></p> <ul> <li>6 Months</li> </ul> <p><strong>Approved Training Allowance:</strong></p> <ul> <li>Fresh Graduates: S$2500</li> <li>Non-Mature Mid-Career Individuals: $2800</li> <li>Mature Mid-Career Individuals: $3200</li> </ul> <p>This position is open for both recent graduates and mid-career individuals (mature and non mature). Graduates interested in this position should possess a University Degree Qualification. Mid-career individuals from any qualification level can apply.</p></div>'
+
+
+extracting_job_desc_named(txt, ["Descriptions", "Competencies"])
 
 
 def main():
     naive_extraction_obj = extraction_text(mcf_df["Description"], extracting_job_desc_naive)
+    named_extraction_obj = extraction_text(mcf_df["Description"], extracting_job_desc_named)
 
 
 if __name__ == "__main__":
