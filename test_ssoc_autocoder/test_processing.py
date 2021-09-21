aefd7f1d72fc173a729f6ec5145689a9256a30e6
benjamin-aw-93 <benjaminaw93@hotmail.com>
2021-09-21 20:32:47 +0800

    Add Fixture to test script

--- test_ssoc_autocoder/test_processing.py
+++ test_ssoc_autocoder/test_processing.py
@@ -2,10 +2,20 @@ import pytest
 from ssoc_autocoder.processing import *
 
 
-class TestProcessing:
+@pytest.fixture
+def nlp():
+    return spacy.load('en_core_web_lg')
 
-    def test_remove_prefix(self):
-        assert remove_prefix("123 hello", ['123']) == "hello"
+
+def test_remove_prefix():
+    assert remove_prefix("123 hello", ['123']) == "hello"
+    assert remove_prefix("123 hello", ['@23']) == "123 hello"
+
+
+def test_check_if_first_word_is_verb(nlp):
+    assert check_if_first_word_is_verb('proven success', nlp) == False
+    assert check_if_first_word_is_verb('review success', nlp) == True
+    assert check_if_first_word_is_verb('acting on this', nlp) == True
 
 
 if __name__ == '__main__':
