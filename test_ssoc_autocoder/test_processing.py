b8e5106ce92c7cd3dfc112c37c6ce86d7c612550
benjamin-aw-93 <benjaminaw93@hotmail.com>
2021-09-21 18:10:37 +0800

    Add first test script

--- test_ssoc_autocoder/test_processing.py
+++ test_ssoc_autocoder/test_processing.py
@@ -1,2 +1,12 @@
 import pytest
-from .ssoc-autocoder import *
+from ssoc_autocoder.processing import *
+
+
+class TestProcessing:
+
+    def test_remove_prefix(self):
+        assert remove_prefix("123 hello", ['123']) == "hello"
+
+
+if __name__ == '__main__':
+    pytest.main()
