1524407187b67f31d3effce50f3f5a38adae37e6
shaunkhoo <khoowushaun1994@gmail.com>
2021-10-04 17:47:01 +0800

    Clean up training functions
    Fix bugs with the training functions causing it
    to crash, worked on adding more useful printouts
    during the training process.
    
    Function to generate predictions is ready now.

--- ssoc_autocoder/train.py
+++ ssoc_autocoder/train.py
@@ -3,6 +3,8 @@ import pandas as pd
 from copy import deepcopy
 import numpy as np
 from sklearn.model_selection import train_test_split
+import time
+from datetime import datetime
 
 # Neural network libraries
 import torch
@@ -60,28 +62,34 @@ def generate_encoding(reference_data, ssoc_colname = 'SSOC 2020'):
 
 def encode_dataset(data,
                    encoding,
-                   ssoc_colname = 'SSOC 2020'):
+                   colnames):
     """
     Encodes SSOCs into indices for the training data.
 
     Uses the generated encoding to encode the SSOCs into indices
-    at each digit level for PyTorch training.
+    at each digit level for PyTorch training. Drops any other
+    columns that are not relevant to the model training process.
 
     Args:
         data: Pandas dataframe of the training data with the correct SSOC
         encoding: Encoding for each SSOC level
-        ssoc_colname: Name of the SSOC column
+        colnames: Dictionary of all column names
 
     Returns:
         Pandas dataframe with each digit SSOC encoded correctly
     """
 
-    # Create a copy of the dataframe
-    encoded_data = deepcopy(data)
+    # Create a copy of the dataframe and filter out any 'X' SSOCs
+    data = data[data[colnames['SSOC']].notnull()]
+    encoded_data = deepcopy(data)[~data[colnames['SSOC']].str.contains('X')]
+
+    # Subset the dataframe for only the required 2 columns
+    encoded_data = encoded_data[[colnames['job_description'], colnames['SSOC']]]
+    encoded_data.columns = ['Text', 'SSOC']
 
     # For each digit, encode the SSOC correctly
     for ssoc_level, encodings in encoding.items():
-        encoded_data[ssoc_level] = encoded_data[ssoc_colname].astype('str').str.slice(0, int(ssoc_level[5])).replace(encodings['ssoc_idx'])
+        encoded_data[ssoc_level] = encoded_data['SSOC'].astype('str').str.slice(0, int(ssoc_level[5])).replace(encodings['ssoc_idx'])
 
     return encoded_data
 
@@ -92,7 +100,7 @@ class SSOC_Dataset(Dataset):
     """
 
     # Define the class attributes
-    def __init__(self, dataframe, tokenizer, max_len):
+    def __init__(self, dataframe, tokenizer, max_len, colnames):
         self.len = len(dataframe)
         self.data = dataframe
         self.tokenizer = tokenizer
@@ -102,7 +110,8 @@ class SSOC_Dataset(Dataset):
     def __getitem__(self, index):
 
         # Extract the text
-        text = self.data[colnames['job_description']][index]
+        # NEED TO FIX THIS
+        text = self.data.Text[index]
 
         # Pass in the data into the tokenizer
         inputs = self.tokenizer(
@@ -135,6 +144,7 @@ class SSOC_Dataset(Dataset):
         return self.len
 
 def prepare_data(encoded_data,
+                 tokenizer,
                  colnames,
                  parameters):
     """
@@ -151,29 +161,24 @@ def prepare_data(encoded_data,
 
     """
 
-    # Split the dataset into training and testing
-    training_data, testing_data = train_test_split(encoded_data,
-                                                   test_size = 0.2,
-                                                   random_state = 2021)
-
-    # Reset the indices as PyTorch refers to the indices when iterating
+    # Split the dataset into training and validation with a 20% split
+    training_data, validation_data = train_test_split(encoded_data,
+                                                      test_size = 0.2,
+                                                      random_state = 2021)
     training_data.reset_index(drop = True, inplace = True)
-    testing_data.reset_index(drop = True, inplace = True)
-
-    # Load the DistilBertTokenizer
-    tokenizer = DistilBertTokenizer.from_pretrained(parameters['pretrained_model'])
+    validation_data.reset_index(drop = True, inplace = True)
 
     # Creating the dataset and dataloader for the neural network
-    training_loader = DataLoader(SSOC_Dataset(training_data, tokenizer, parameters['sequence_max_length']),
+    training_loader = DataLoader(SSOC_Dataset(training_data, tokenizer, parameters['sequence_max_length'], colnames),
                                  batch_size = parameters['training_batch_size'],
                                  num_workers = parameters['num_workers'],
                                  shuffle = True)
-    testing_loader = DataLoader(SSOC_Dataset(testing_data, tokenizer, parameters['sequence_max_length']),
-                                batch_size = parameters['training_batch_size'],
-                                num_workers = parameters['num_workers'],
-                                shuffle = True)
+    validation_loader = DataLoader(SSOC_Dataset(validation_data, tokenizer, parameters['sequence_max_length'], colnames),
+                                   batch_size = parameters['training_batch_size'],
+                                   num_workers = parameters['num_workers'],
+                                   shuffle = True)
 
-    return training_loader, testing_loader
+    return training_loader, validation_loader
 
 def prepare_model(encoding, parameters):
     """
@@ -259,33 +264,40 @@ def prepare_model(encoding, parameters):
 
     return model, loss_function, optimizer
 
-
-import time
-from datetime import datetime
-
-
-def calculate_accu(big_idx, targets):
+def calculate_accuracy(big_idx, targets):
     n_correct = (big_idx == targets).sum().item()
     return n_correct
 
+def train_model(model,
+                loss_function,
+                optimizer,
+                training_loader,
+                validation_loader,
+                parameters):
 
-def train_model(model, loss_function, optimizer, epochs):
-    start_time = time.time()
-    now = datetime.now()
-    current_time = now.strftime("%d %b %Y - %H:%M:%S")
-    print("Training started on:", current_time)
+    # Start the timer
+    start_time = datetime.now()
+    print(f"Training started on: {start_time.strftime('%d %b %Y - %H:%M:%S')}")
+    print("====================================================================")
 
-    for epoch in range(epochs):
+    # Iterate over each training epoch
+    for epoch_num in range(1, parameters['epochs']+1):
+
+        # Start the timer for the epoch
+        epoch_start_time = datetime.now()
+        print(f"> Epoch {epoch_num} started on: {epoch_start_time.strftime('%d %b %Y - %H:%M:%S')}")
+        print("--------------------------------------------------------------------")
+
         tr_loss = 0
-        n_correct = 0
+        tr_n_correct = 0
         nb_tr_steps = 0
         nb_tr_examples = 0
 
-        epoch_start_time = time.time()
-        batch_start_time = time.time()
-
         # Set the NN to train mode
         model.train()
+
+        # Start the timer for the batch
+        batch_start_time = datetime.now()
 
         # Iterate over each batch
         for batch, data in enumerate(training_loader):
@@ -315,45 +327,125 @@ def train_model(model, loss_function, optimizer, epochs):
                     loss += level_loss * parameters['loss_weights'][ssoc_level]
 
             # Use the deepest level predictions to calculate accuracy
+            # Exploit the fact that the last preds object is the deepest level one
             top_probs, top_probs_idx = torch.max(preds.data, dim = 1)
-            n_correct += calculate_accu(top_probs_idx, targets)
-
-            # Calculate the loss
-            #         targets_1d = data['targets_1d'].to(device, dtype = torch.long)
-            #         targets_2d = data['targets_2d'].to(device, dtype = torch.long)
-            #         loss1 = loss_function(preds_1d, targets_1d)
-            #         loss2 = loss_function(preds_2d, targets_2d)
-            #         loss = loss1*5 + loss2
+            tr_n_correct += calculate_accuracy(top_probs_idx, targets)
 
             # Add this batch's loss to the overall training loss
             tr_loss += loss.item()
 
+            # Keep count for the batch steps and number of examples
             nb_tr_steps += 1
             nb_tr_examples += targets.size(0)
 
+            # Run backpropagation
             optimizer.zero_grad()
             loss.backward()
-            # # When using GPU
             optimizer.step()
 
-            if (batch + 1) % 500 == 0:
+            # For every X number of steps, print some information
+            batch_size_printing = 50
+            if (batch + 1) % batch_size_printing == 0:
                 loss_step = tr_loss / nb_tr_steps
-                accu_step = (n_correct * 100) / nb_tr_examples
-                print(f"Training Loss per 500 steps: {loss_step}")
-                print(f"Training Accuracy per 500 steps: {accu_step}")
-                print(f"Batch of 500 took {(time.time() - batch_start_time) / 60:.2f} mins")
-                batch_start_time = time.time()
+                accu_step = (tr_n_correct * 100) / nb_tr_examples
+                print(f">> Training Loss per {batch_size_printing} steps: {loss_step:.4f} ")
+                print(f">> Training Accuracy per {batch_size_printing} steps: {accu_step:.2f}%")
+                print(f">> Batch of {batch_size_printing} took {(datetime.now() - batch_start_time).total_seconds() / 60:.2f} mins")
+                batch_start_time = datetime.now()
 
-        print(f'The Total Accuracy for Epoch {epoch}: {(n_correct * 100) / nb_tr_examples}')
-        epoch_loss = tr_loss / nb_tr_steps
-        epoch_accu = (n_correct * 100) / nb_tr_examples
-        print(f"Training Loss Epoch: {epoch_loss}")
-        print(f"Training Accuracy Epoch: {epoch_accu}")
-        print(f"Epoch training time: {(time.time() - epoch_start_time) / 60:.2f} mins")
+        # Set the model to evaluation mode for the validation
+        model.eval()
 
-    print(f"Total training time: {(time.time() - start_time) / 60:.2f} mins")
-    now = datetime.now()
-    current_time = now.strftime("%d %b %Y - %H:%M:%S")
-    print("Training ended on:", current_time)
+        va_n_correct = 0
+        va_loss = 0
+        nb_va_steps = 0
+        nb_va_examples = 0
+
+        # Disable the calculation of gradients
+        with torch.no_grad():
+
+            # Iterate over each batch
+            for batch, data in enumerate(validation_loader):
+
+                # Extract the data
+                ids = data['ids'].to(parameters['device'], dtype = torch.long)
+                mask = data['mask'].to(parameters['device'], dtype = torch.long)
+
+                # Run the forward prop
+                predictions = model(ids, mask)
+
+                # Iterate through each SSOC level
+                for ssoc_level, preds in predictions.items():
+
+                    # Extract the correct target for the SSOC level
+                    targets = data[ssoc_level].to(parameters['device'], dtype = torch.long)
+
+                    # Compute the loss function using the predictions and the targets
+                    level_loss = loss_function(preds, targets)
+
+                    # Initialise the loss variable if this is the 1D level
+                    # Else add to the loss variable
+                    # Note the weights on each level
+                    if ssoc_level == 'SSOC_1D':
+                        loss = level_loss * parameters['loss_weights'][ssoc_level]
+                    else:
+                        loss += level_loss * parameters['loss_weights'][ssoc_level]
+
+                # Use the deepest level predictions to calculate accuracy
+                # Exploit the fact that the last preds object is the deepest level one
+                top_probs, top_probs_idx = torch.max(preds.data, dim = 1)
+                va_n_correct += calculate_accuracy(top_probs_idx, targets)
+
+                # Add this batch's loss to the overall training loss
+                va_loss += loss.item()
+
+                # Keep count for the batch steps and number of examples
+                nb_va_steps += 1
+                nb_va_examples += targets.size(0)
+
+        print("--------------------------------------------------------------------")
+        epoch_tr_loss = tr_loss / nb_tr_steps
+        epoch_va_loss = va_loss / nb_va_steps
+        epoch_tr_accu = (tr_n_correct * 100) / nb_tr_examples
+        epoch_va_accu = (va_n_correct * 100) / nb_va_examples
+        print(f"> Epoch {epoch_num} Loss = \tTraining: {epoch_tr_loss:.3f}  \tValidation: {epoch_va_loss:.3f}")
+        print(f"> Epoch {epoch_num} Accuracy = \tTraining: {epoch_tr_accu:.2f}%  \tValidation: {epoch_va_accu:.2f}%")
+        print(f"> Epoch {epoch_num} took {(datetime.now() - epoch_start_time).total_seconds() / 60:.2f} mins")
+        print("====================================================================")
+
+    end_time = datetime.now()
+    print("Training ended on:", end_time.strftime("%d %b %Y - %H:%M:%S"))
+    print(f"Total training time: {(end_time - start_time).total_seconds() / 3600:.2f} hours")
 
     return
+
+def generate_prediction(model,
+                        tokenizer,
+                        text,
+                        target,
+                        parameters):
+
+    tokenized = tokenizer(
+        text = text,
+        text_pair = None,
+        add_special_tokens = True,
+        max_length = parameters['sequence_max_length'],
+        padding = 'max_length',
+        return_token_type_ids = True,
+        truncation = True
+    )
+    test_ids = torch.tensor([tokenized['input_ids']], dtype = torch.long)
+    test_mask = torch.tensor([tokenized['attention_mask']], dtype = torch.long)
+
+    model.eval()
+    preds = model(test_ids, test_mask)
+    m = torch.nn.Softmax(dim = 1)
+
+    predicted_1D = encoding['SSOC_1D']['idx_ssoc'][np.argmax(preds["SSOC_1D"].detach().numpy())]
+    predicted_1D_proba = np.max(m(preds['SSOC_1D']).detach().numpy())
+    predicted_2D = encoding['SSOC_2D']['idx_ssoc'][np.argmax(preds["SSOC_2D"].detach().numpy())]
+    predicted_2D_proba = np.max(m(preds['SSOC_2D']).detach().numpy())
+
+    print(f"Target: {target}")
+    print(f"Model predicted 1D: {predicted_1D} ({predicted_1D_proba * 100:.2f}%)")
+    print(f"Model predicted 2D: {predicted_2D} ({predicted_2D_proba * 100:.2f}%)")
