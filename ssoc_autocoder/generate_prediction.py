94820daa5612dfc0a99af6e41ddba893dd748122
benjamin-aw-93 <benjaminaw93@hotmail.com>
2022-01-05 22:17:03 +0800

    Update generate predictions
    Adding in encodings for the model to take reference to

--- ssoc_autocoder/generate_prediction.py
+++ ssoc_autocoder/generate_prediction.py
@@ -1,40 +1,56 @@
 # Importing libraries
 import json
 import torch
+import numpy as np
 from transformers import DistilBertTokenizer
 import model_training
 
 parameters = {
-    'architecture': 'hierarchical',
-    'sequence_max_length': 512,
-    'max_level': 5,
-    'training_batch_size': 32,
-    'validation_batch_size': 32,
-    'epochs': 40,
-    'learning_rate': 0.0005,
-    # 'distilbert-base-uncased',
-    'pretrained_model': 'D:\\Users\\benjamin\\my_enviro\\Models\\distilbert-tokenizer',
-    'model_parameters_path': "D:\\Users\\benjamin\\my_enviro\\Models\\autocoder-30dec-pretrained-60epoch.pt",
-    'model_on_local_disk': True,
-    'num_workers': 4,
-    'loss_weights': {
-        'SSOC_1D': 20,
-        'SSOC_2D': 5,
-        'SSOC_3D': 3,
-        'SSOC_4D': 2,
-        'SSOC_5D': 1
-    },
-    'device': 'cpu'
+    'pretrained_tokeniser': 'D:\\Users\\benjamin\\my_enviro\\Models\\distilbert-tokenizer-pretrained',
+    'model_parameters_path': 'D:\\Users\\benjamin\\my_enviro\\Models\\autocoder-4jan-pretrained3epoch-60epoch.pt',
+    'device': 'cpu',
+    'sequence_max_length': 512
 }
 
 model_parameters = {
-    'pretrained_model': 'D:\\Users\\benjamin\\my_enviro\\Models\\distilbert-tokenizer',
+    'pretrained_model': 'D:\\Users\\benjamin\\my_enviro\Models\\mcf-pretrained-3epoch',
     'model_on_local_disk': True,
     'max_level': 5
 }
 
 
-def generate_prediction(model, tokenizer, text, target, parameters):
+def import_ssoc_idx_encoding(filename):
+    """
+    Imports the SSOC-index encoding and formats it correctly.
+    """
+
+    # Open and load the JSON file as a dictionary
+    with open(filename) as json_file:
+        unformatted_dict = json.load(json_file)
+
+    # Initialise the output dictionary object
+    output_dict = {}
+
+    # Iterate through each SSOC level in the encoding
+    for ssoc_level in unformatted_dict.keys():
+
+        # Initialise the key:value pair for each SSOC level
+        output_dict[ssoc_level] = {
+            'ssoc_idx': {},
+            'idx_ssoc': {}
+        }
+
+        # Directly copy of the ssoc_idx object since this is formatted correctly
+        output_dict[ssoc_level]['ssoc_idx'] = unformatted_dict[ssoc_level]['ssoc_idx']
+
+        # For the idx_ssoc object, format the key as a number instead of a string
+        for key, value in unformatted_dict[ssoc_level]['idx_ssoc'].items():
+            output_dict[ssoc_level]['idx_ssoc'][int(key)] = value
+
+    return output_dict
+
+
+def generate_prediction(model, tokenizer, text, target, encoding, parameters):
     tokenized = tokenizer(
         text=text,
         text_pair=None,
@@ -72,10 +88,20 @@ def generate_prediction(model, tokenizer, text, target, parameters):
     print(f"Model predicted 5D: {predicted_5D} ({predicted_5D_proba*100:.2f}%)")
 
 
-weight = torch.load(parameters['model_parameters_path'], map_location=torch.device('cpu'))
+weight = torch.load(parameters['model_parameters_path'],
+                    map_location=torch.device(parameters['device']))
+
 model = model_training.HierarchicalSSOCClassifier(model_parameters)
+
 model.load_state_dict(weight)
-tokenizer = DistilBertTokenizer.from_pretrained('../Models/distilbert-tokenizer')
+
+tokenizer = DistilBertTokenizer.from_pretrained(parameters['pretrained_tokeniser'])
+
 text = "Assist the Resident Engineer in carrying out supervision works on site and enforce site quality control while coordinating with client / main contractor / sub-contractor. Liaise with site representatives from other trades during executive of site works to ensure smooth and successful completion of structural works. Assist the Resident Engineer in keeping and maintaining site record on progress of works, instructions given to Contractors, materials delivered to site, daily activities and weather conditions at site, number of visitors at site, any other information as attached and required under the Malaysian Laws and Regulations. Review and study all documents and drawings issued for construction, and ensure coordination of details with other trades. Carry out other duties as directed by the Qualified Person or his representative."
+
 test_target = 31124
-generate_prediction(model, tokenizer, text, test_target, parameters)
+
+encoding = import_ssoc_idx_encoding(
+    "D:\\Users\\benjamin\\my_enviro\\Data\\Reference\\ssoc-idx-encoding.json")
+
+generate_prediction(model, tokenizer, text, test_target, encoding, parameters)
