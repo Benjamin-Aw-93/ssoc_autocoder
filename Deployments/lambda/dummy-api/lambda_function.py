7711575f2931f322c483f729d6e0930bc3b64c15
shaunkhoo <shaun@dsaid.gov.sg>
2022-01-10 22:29:30 +0800

    Add dummy-api lambda function
    Add new folder to version control all AWS deployments.
    Create simple Python script and deploy on AWS to provide
    dummy API for Ben to work with when building out the
    front-end.

---
+++ Deployments/lambda/dummy-api/lambda_function.py
@@ -0,0 +1,67 @@
+import json
+import logging
+import random
+import hashlib
+import urllib3
+
+http = urllib3.PoolManager()
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    """
+    Accepts an action and a number, performs the specified action on the number,
+    and returns the result.
+    :param event: The event dict that contains the parameters sent when the function
+                  is invoked.
+    :param context: The context in which the function is called.
+    :return: The result of the specified action.
+    """
+    logger.info('Event: %s', event)
+
+    with open('dummy_data.json') as json_file:
+        dummy_data = json.load(json_file)
+
+    # Select one of the dummy data randomly
+    data = dummy_data[random.randint(0,9)]
+
+    # Retrieve the MCF job ad ID and overwriting it with one of our samples
+    mcf_job_ad_id = event['queryStringParameters']['job_id']
+    logger.info('Queried MCF job ad ID: %s', mcf_job_ad_id)
+    mcf_job_ad_id = data['MCF_Job_Ad_ID']
+    logger.info('Replaced with MCF job ad ID: %s', mcf_job_ad_id)
+
+    # Call the MCF API
+    mcf_uuid = hashlib.md5(mcf_job_ad_id.encode()).hexdigest()
+    resp = http.request('GET', f'https://api.mycareersfuture.gov.sg/v2/jobs/{mcf_uuid}')
+    
+    # Error handling if an invalid MCF job ad ID is provided
+    if resp.status != 200 or json.loads(resp.data) == {'message': 'UUID is not found in the database.'}:
+        response = {
+            'statusCode': 400,
+            'body': json.dumps({'Error': 'Error calling the MCF Jobs API with the provided MCF job ad ID. Please check that you have a valid MCF job ad ID and call this API again.'})
+        }
+        return response
+
+    # If not, we extract the job description
+    mcf_job_desc = json.loads(resp.data)['description']
+    
+    # Compile the output into a dictionary
+    output = {
+        'job_id': mcf_job_ad_id,
+        'job_desc': mcf_job_desc,
+        'correct_ssoc': data['Correct_SSOC_2020'],
+        'top_10_preds': data['SSOC_5D_Top_10_Preds'],
+        'top_10_proba': data['SSOC_5D_Top_10_Preds_Proba']
+    }
+
+    # Create the response objective
+    response = {
+        'statusCode': 200,
+        'body': json.dumps(output)
+    }
+    
+    logger.info('Completed query!')
+
+    return response
