fb6f2f1f6d9a90caa2a9e71179b042587e65bbac
hadragon26 <87232777+hadragon26@users.noreply.github.com>
2022-06-02 10:47:03 +0800

    Update json_to_processed_csv.py
    Reformatted lambda function to ensure no function is inside another

--- Deployments/feature/add-data-processing/json_to_processed_csv.py
+++ Deployments/feature/add-data-processing/json_to_processed_csv.py
@@ -8,9 +8,84 @@ from datetime import date, datetime, timedelta
 import requests
 import boto3
 
+def write_to_csv(output):
+    """
+    write the pandas to csv and groups them according to week and year 
+
+    Parameters:
+        output (dictionary) : Dictionary containg the week and year as keys and pandas
+        rows as values 
+
+    Returns:
+        
+    """
+    for dates in output.keys():
+        pd.DataFrame(output[dates]).to_csv("/tmp/raw_" + dates + ".csv", index = False)
+    
+def cleaning_text_and_check(text):
+    
+    """
+    Process job description, put text through each process function, and return results according to precedence.
+
+    Parameters:
+        text: Job descriptions text
+
+    Returns:
+        Extracted text
+    """
+    
+    cleaned_text = process_text(text)
+    
+    # add in additional check for proper sentences
+    
+    return cleaned_text
 
 
+def output_individual_files(path):
+    
+    """
+    Opens the csv files and processes
+    the text in the description cloumn
 
+    Parameters:
+        path (str): the path where the unprocessed files
+        are found and where the processed files 
+        will be at
+    Returns:
+        
+    """
+
+    lst = os.listdir(path)
+    for filename in lst:
+        if filename.startswith('raw'):
+            
+            now = datetime.now()
+            
+            df = pd.read_csv(path + filename)
+            df['description']= df['description'].apply(cleaning_text_and_check)
+            df['Processed date'] = now
+            df.to_csv(path+'processed'+filename[3:], index=False)
+
+
+def get_object(file_name, yesterday):
+    
+    """
+    Checks if the the files are the latest files 
+
+    Parameters:
+        file_name (str): file name that you want to check about
+        yesterday (date): Date to input 
+
+    Returns:
+        True or False depending if it is modified
+        within the period between today and yesterday
+    """
+    
+    try:
+        obj = file_name.get(IfModifiedSince=yesterday)
+        return obj['Body']
+    except:
+        False
 
 def lambda_handler(event, context):
 
@@ -26,26 +101,16 @@ def lambda_handler(event, context):
     bucket ='raw-json-mcf'
     buck=s3.Bucket(bucket)
     
-    #function for checking if the file is up to date based on the yesterday date
-    def get_object(file_name):
-        try:
-            obj = file_name.get(IfModifiedSince=yesterday)
-            return obj['Body']
-        except:
-            False
+    
             
     for s3_object in buck.objects.all():
         
         filename=s3_object.key
         
-        # to check if the file is the latest added file
-        if get_object(s3_object):
+        if get_object(s3_object,yesterday):
         
             try:
-
-                #downlad the file into tmp
                 buck.download_file(filename, f'/tmp/'+filename)
-
             except Exception as e:
                 print(e)
                 print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
@@ -54,33 +119,6 @@ def lambda_handler(event, context):
             
             print(f'{filename} is too old')
       
-    
-    def write_to_csv(output):
-    
-        for dates in output.keys():
-            pd.DataFrame(output[dates]).to_csv("/tmp/raw_" + dates + ".csv", index = False)
-    
-    def cleaning_text_and_check(text):
-        
-        cleaned_text = process_text(text)
-        
-        # add in additional check for proper sentences
-        
-        return cleaned_text
-
-
-    def output_individual_files(path):
-
-        lst = os.listdir(path)
-        for filename in lst:
-            if filename.startswith('raw'):
-                
-                now = datetime.now()
-                
-                df = pd.read_csv(path + filename)
-                df['description']= df['description'].apply(cleaning_text_and_check)
-                df['Processed date'] = now
-                df.to_csv(path+'processed'+filename[3:], index=False)
 
     
     output = extract_and_split(path)
@@ -101,7 +139,7 @@ def lambda_handler(event, context):
     except Exception as e:
         print(e)
     
-    # signal that it is completed
+
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
