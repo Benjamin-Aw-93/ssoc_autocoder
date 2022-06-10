214fdd2391b61340c329ea5f908f54258236e238
hadragon26 <87232777+hadragon26@users.noreply.github.com>
2022-06-10 10:51:37 +0800

    Check if the json_object for the job id exists
    Included the check if the json object job_id is already in the bucket

--- Deployments/feature/add-data-processing/json_object_from_mcf_job_id.py
+++ Deployments/feature/add-data-processing/json_object_from_mcf_job_id.py
@@ -23,6 +23,22 @@ def lambda_handler(event, context):
     #base url for the api
     base_url = 'https://api.mycareersfuture.gov.sg/v2/jobs'
 
+    
+    #check if the job id is already in the bucket
+    try:
+            
+        s3.meta.client.download_file(dir_bucket, f'{mcf_job_id}.json', '/tmp/'+f'{mcf_job_id}.json')
+        
+        response = {
+        'statusCode': 400,
+        'body': json.dumps(f" Already have {mcf_job_id}.json in {dir_bucket} bukcet ")
+    }
+        return response
+        
+    except:
+        
+        pass
+    
     #api request
     req = requests.get(base_url + "/" + uuid)
   
@@ -36,6 +52,8 @@ def lambda_handler(event, context):
     
     #dump the json object ito the bucket
     else:
+        
+        
                          
         try:                    
             with open(f'/tmp/{mcf_job_id}.json', 'w') as file:
@@ -55,7 +73,7 @@ def lambda_handler(event, context):
     #return successful response 
     response = {
         'statusCode': 200,
-        'body': json.dumps(f'{mcf_job_id}.json in {dir_bucket} bukcet')
+        'body': json.dumps(f' Successfully put {mcf_job_id}.json in {dir_bucket} bukcet')
     }
             
     return response
