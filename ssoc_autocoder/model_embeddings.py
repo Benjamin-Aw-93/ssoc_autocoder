1398ba21f45e167d045dd6ce2e96aeb523e03040
Yee Sen :) <ystan98@hotmail.com>
2023-08-30 12:22:54 +0800

    Update model_embeddings.py
    Edited code to allow user to select the model they want to use

--- ssoc_autocoder/model_embeddings.py
+++ ssoc_autocoder/model_embeddings.py
@@ -1,6 +1,10 @@
-from transformers import DistilBertTokenizer, AutoModel
+from transformers import DistilBertTokenizer, AutoModel,DistilBertModel
+from transformers import RobertaTokenizer, RobertaModel
+from transformers import AutoTokenizer, AutoModelForMaskedLM
 import torch
 import pandas as pd
+import model_training
+from transformers import BertTokenizer, BertModel
 from torch.utils.data import DataLoader
 def generate_embeddings(model, 
                         tokenizer, 
@@ -29,20 +33,31 @@ def generate_embeddings(model,
                              pin_memory=False)
     
     def embeddings_iterator(data_loader):
-        output = []
-        for batch in data_loader:
-            tokenized_text = tokenizer(batch,
-                                    text_pair=None, 
-                                    add_special_tokens=True, 
-                                    max_length=512,
-                                    padding='max_length',
-                                    return_token_type_ids=True,
-                                    truncation=True,
-                                    return_tensors='pt'
-                                )
-            del tokenized_text['token_type_ids']
-            output.append(model(**tokenized_text).last_hidden_state[:, 0])
-        return torch.cat(output, dim=0)
+        try:
+            output = []
+            for batch in data_loader:
+                tokenized_text = tokenizer(batch,
+                                        text_pair=None, 
+                                        add_special_tokens=True, 
+                                        max_length=512,
+                                        padding='max_length',
+                                        return_token_type_ids=True,
+                                        truncation=True,
+                                        return_tensors='pt'
+                                    )
+                del tokenized_text['token_type_ids']
+                output.append(model(**tokenized_text).last_hidden_state[:, 0])
+            return torch.cat(output, dim=0)
+        except:
+            output=[]
+
+            for batch in data_loader:
+                encoded_input = tokenizer(batch, return_tensors='pt', )
+                output = model(**encoded_input)
+
+                output.append(model(**tokenized_text).last_hidden_state[:, 0])
+
+            return torch.cat(output, dim=0)
     
     title_embeddings = embeddings_iterator(title_loader)
     text_embeddings = embeddings_iterator(text_loader)
@@ -51,16 +66,36 @@ def generate_embeddings(model,
 
 if __name__ == "__main__":
     # load artifacts, data, then generate and store embeddings
-    lang_model_path = 'artifacts/lang-model/'
-    tokenizer_path = 'artifacts/distilbert-tokenizer-pretrained-7epoch'
-    lang_model = AutoModel.from_pretrained(lang_model_path)
-    tokenizer = DistilBertTokenizer.from_pretrained(tokenizer_path)
+    # lang_model_path = 'artifacts/lang-model/'
+    # tokenizer_path = 'artifacts/distilbert-tokenizer-pretrained-7epoch'
+    # lang_model = AutoModel.from_pretrained(lang_model_path)
+    # tokenizer = DistilBertTokenizer.from_pretrained(tokenizer_path)
+
+    language_models = {'DistilBERT':[DistilBertTokenizer.from_pretrained('./artifacts/distilbert-tokenizer-pretrained-7epoch'),DistilBertModel.from_pretrained('./artifacts/basemodel')],
+                       'RoBERTa-base':[RobertaTokenizer.from_pretrained('roberta-base'),RobertaModel.from_pretrained('roberta-base')]
+                       }
+
+
     # load data
-    train = pd.read_csv('data/inputs/subtest.csv')
+    # train = pd.read_csv('data/inputs/subtest.csv')
+    train = pd.read_csv('./data/subset.csv')
     title = train['job_title']
     text = train['job_description']
 
-    title_embeddings, text_embeddings = generate_embeddings(lang_model, tokenizer, title, text)
+
+    # chooose from DistilBERT, RoBERTa, etc
+    selected = 'RoBERTa-base'
+
+    model = language_models[selected][1]
+    tokenizer = language_models[selected][0]
+
+
+    title_embeddings, text_embeddings = generate_embeddings(model, tokenizer, title, text)
+
+    print(title_embeddings)
+    
+
+
     # save the embeddings
-    torch.save(title_embeddings, f'data/embeddings/embeddings_title_{lang_model.__class__.__name__}.pt')
-    torch.save(text_embeddings, f'data/embeddings/embeddings_text_{lang_model.__class__.__name__}.pt')
+    # torch.save(title_embeddings, f'data/embeddings/embeddings_title_{lang_model.__class__.__name__}.pt')
+    # torch.save(text_embeddings, f'data/embeddings/embeddings_text_{lang_model.__class__.__name__}.pt')
