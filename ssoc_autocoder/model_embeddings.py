c223b79616c904c40de2160ba128429647129b0b
unknown <joel_koo@mom.gov.sg>
2023-09-04 16:38:31 +0800

    Modified model_embeddings to use any language model for generating embeddings

--- ssoc_autocoder/model_embeddings.py
+++ ssoc_autocoder/model_embeddings.py
@@ -1,11 +1,9 @@
-from transformers import DistilBertTokenizer, AutoModel,DistilBertModel
-from transformers import RobertaTokenizer, RobertaModel
-from transformers import AutoTokenizer, AutoModelForMaskedLM
+from transformers import AutoModel, AutoTokenizer
 import torch
 import pandas as pd
-import model_training
-from transformers import BertTokenizer, BertModel
 from torch.utils.data import DataLoader
+from tqdm import tqdm
+
 def generate_embeddings(model, 
                         tokenizer, 
                         title,
@@ -20,22 +18,10 @@ def generate_embeddings(model,
     #     raise TypeError("Please enter a string for the 'text' argument.")
     # if type(title) != str:
     #     raise TypeError("Please enter a string for the 'title' argument.")
-    title_loader = DataLoader(title,
-                             batch_size=32,
-                             num_workers=4,
-                             shuffle=False,
-                             pin_memory=False)
-    
-    text_loader = DataLoader(text,
-                             batch_size=32,
-                             num_workers=4,
-                             shuffle=False,
-                             pin_memory=False)
-    
     def embeddings_iterator(data_loader):
         try:
             output = []
-            for batch in data_loader:
+            for batch in tqdm(data_loader):
                 tokenized_text = tokenizer(batch,
                                         text_pair=None, 
                                         add_special_tokens=True, 
@@ -46,7 +32,7 @@ def generate_embeddings(model,
                                         return_tensors='pt'
                                     )
                 del tokenized_text['token_type_ids']
-                output.append(model(**tokenized_text).last_hidden_state[:, 0])
+                output.append(model(**tokenized_text).last_hidden_state[:, 0].detach())
             return torch.cat(output, dim=0)
         except:
             output=[]
@@ -55,46 +41,47 @@ def generate_embeddings(model,
                 encoded_input = tokenizer(batch, return_tensors='pt', )
                 output = model(**encoded_input)
 
-                output.append(model(**tokenized_text).last_hidden_state[:, 0])
+                output.append(model(**tokenized_text).last_hidden_state[:, 0].detach())
 
             return torch.cat(output, dim=0)
+    title_loader = DataLoader(title,
+                             batch_size=16,
+                             num_workers=4,
+                             shuffle=False,
+                             pin_memory=False)
     
     title_embeddings = embeddings_iterator(title_loader)
+    del title_loader
+
+    text_loader = DataLoader(text,
+                             batch_size=16,
+                             num_workers=4,
+                             shuffle=False,
+                             pin_memory=False)
     text_embeddings = embeddings_iterator(text_loader)
-    
+    del text_loader
+
     return title_embeddings, text_embeddings
 
 if __name__ == "__main__":
     # load artifacts, data, then generate and store embeddings
-    # lang_model_path = 'artifacts/lang-model/'
-    # tokenizer_path = 'artifacts/distilbert-tokenizer-pretrained-7epoch'
-    # lang_model = AutoModel.from_pretrained(lang_model_path)
-    # tokenizer = DistilBertTokenizer.from_pretrained(tokenizer_path)
-
-    language_models = {'DistilBERT':[DistilBertTokenizer.from_pretrained('./artifacts/distilbert-tokenizer-pretrained-7epoch'),DistilBertModel.from_pretrained('./artifacts/basemodel')],
-                       'RoBERTa-base':[RobertaTokenizer.from_pretrained('roberta-base'),RobertaModel.from_pretrained('roberta-base')]
+    # Sept 2023: loading models directly from huggingface. Will include the version for customised models
+    # where we load our own re-trained models
+    # example where we embed with a bert model:
+    language_model_name = "bert-base-uncased"
+    tokenizer_name = "bert-base-uncased"
+    huggingface_model = {'tokenizer': tokenizer_name,
+                        'model': language_model_name
                        }
-
+    language_model = AutoModel.from_pretrained(huggingface_model['model'])
+    tokenizer = AutoTokenizer.from_pretrained(huggingface_model['tokenizer'])
 
     # load data
-    # train = pd.read_csv('data/inputs/subtest.csv')
-    train = pd.read_csv('./data/subset.csv')
+    train = pd.read_csv('data/inputs/train.csv')
     title = train['job_title']
     text = train['job_description']
 
-
-    # chooose from DistilBERT, RoBERTa, etc
-    selected = 'RoBERTa-base'
-
-    model = language_models[selected][1]
-    tokenizer = language_models[selected][0]
-
-
-    title_embeddings, text_embeddings = generate_embeddings(model, tokenizer, title, text)
-
-    print(title_embeddings)
-    
-
+    title_embeddings, text_embeddings = generate_embeddings(language_model, tokenizer, title, text)
 
     # save the embeddings
     # torch.save(title_embeddings, f'data/embeddings/embeddings_title_{lang_model.__class__.__name__}.pt')
